cde177ab151b67595ffe08c3511fecc158025eda
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-07-20 21:14:25 +0100

    fix: solve some design issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         soft: '0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)',
       },
       backgroundImage: {
-        'gradient-148': 'linear-gradient(140deg, #f9fafb 0%, #d2d6db 100%)',
+        'gradient-148': 'linear-gradient(140deg, #F1F3F4 0%, #DBDFE4 100%)',
       },
     },
   },
